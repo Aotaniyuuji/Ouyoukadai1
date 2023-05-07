7ee0d1d17266f91a5e306d62c27f7bcbdeb428d4
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-07 08:29:57 +0000

    [Restart]応用課題1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,6 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
       render :index
     end
   end
@@ -28,7 +27,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book_params)
+    if @book.update()
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
